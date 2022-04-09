b52c9e951f4c892d7fc352556dd24ae050ec3435
Ezechiel ITIMBIEN KAKANA <papaezechiel@gmail.com>
2022-04-08 23:28:27 -0400

    onChange modification

--- src/components/LightSwitchButton.jsx
+++ src/components/LightSwitchButton.jsx
@@ -1,14 +1,13 @@
 import { useState } from "react";
 
-function LightSwitchButton() {
+function LightSwitchButton(props) {
   const [light, setLight] = useState('off');
+  const handleClick = () => setLight(light === "on" ? "off" : "on");
 
   return (
-    <button className="LightSwitchButton">
-      {/* When the state is on */}
-      {light === "on" && <span className="on"><i>💡</i> I'm on!</span>}
-      {/* When the state is off */}
-      {light === "off" && <span className="off"><i>💡</i> I'm off!</span>}
+    <button onClick={handleClick} className="LightSwitchButton">
+      {light === 'on' && <span><i>💡</i> I'm on!</span>}
+      {light === 'off' && <span className="off"><i>💡</i> I'm off!</span>}
     </button>
   );
 }
