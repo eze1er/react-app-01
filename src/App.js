f2b323cd688a020087656e7858773a6fc3419ac3
Ezechiel ITIMBIEN KAKANA <papaezechiel@gmail.com>
2022-04-09 20:27:22 -0400

    light shitch on and off

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 import AngryButton from './components/AngryButton';
 import CounterButton from './components/CounterButton';
@@ -5,13 +6,14 @@ import LightSwitchButton from './components/LightSwitchButton';
 import TextRepeaterButton from './components/TextRepeaterButton';
 
 function App() {
+  const [light, setLight] = useState('off');
   return (
     <div className={`App`}>
       <h1>Fancy Buttons!</h1>
       <section>
         <AngryButton />
         <CounterButton />
-        <LightSwitchButton />
+        <LightSwitchButton light={light} setLight={setLight}/>
         <TextRepeaterButton />
       </section>
     </div> 
